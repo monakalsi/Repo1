ddf295e417580644cc97e7d34383dc7aff2a9bf5
Mona Kalsi <sembhimona90@gmail.com>
2021-05-27 20:48:49 -0700

    hello world in Netbeans

---
+++ Java-oop/classwork/HelloWorld/src/main/java/com/monkalsi/helloworld/Hello.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author sembh
+ * name: Mona Kalsi
+ * email: sembhimona90@gmail.com
+ * date: 2021-05-25
+ * purpose: Practice in an IDE
+ */
+
+//package com.monkalsi.helloworld;
+public class Hello {
+    public static void main(String[]args)
+    {
+        System.out.println("Hello, mona");
+    }
+}
